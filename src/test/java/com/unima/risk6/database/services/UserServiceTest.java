c02f3c544fb4cbea6c2ceac42e85a2133bc0651b
Andrey Stoyanov <astoyano@mail.uni-mannheim.de>
2023-03-09 18:08:58 +0100

    #19 Implemented tests for get,delete and update functions from user service

---
+++ src/test/java/com/unima/risk6/database/services/UserServiceTest.java
@@ -0,0 +1,84 @@
+package com.unima.risk6.database.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import com.unima.risk6.database.configurations.DatabaseConfiguration;
+import com.unima.risk6.database.exceptions.NotFoundException;
+import com.unima.risk6.database.models.User;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for user service
+ *
+ * @author astoyano
+ */
+
+class UserServiceTest {
+
+  private static UserService userService;
+
+  private static Connection connection;
+
+  private static Long userId;
+
+  @BeforeAll
+  static void setUp() {
+    try {
+      DatabaseConfiguration.startDatabaseConfiguration();
+      connection = DatabaseConfiguration.getDatabaseConnection();
+      connection.setAutoCommit(false);
+      userService = DatabaseConfiguration.getUserService();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+
+  @Test
+  @Order(1)
+  void saveUser() {
+    User newUser = new User("astoyano", "password"
+        , "src/main/resources/com/unima/risk6/images/test.png");
+    userId = userService.saveUser(newUser);
+    User databaseUser = userService.getUserById(userId);
+    assertEquals(newUser, databaseUser);
+
+
+  }
+
+  @Test
+  @Order(3)
+  void deleteUserById() {
+
+    userService.deleteUserById(userId);
+    try {
+      userService.getUserById(userId);
+      fail("Should have thrown an not found exception");
+    } catch (NotFoundException e) {
+      final String expected = "User with id {" + userId + "} is not in the database";
+      assertEquals(expected, e.getMessage());
+    }
+
+  }
+
+  @Test
+  @Order(2)
+  void updateUser() {
+    User user = userService.getUserById(userId);
+    user.setUsername("AndreyStoyanov");
+    userService.updateUser(user);
+    User databaseUser = userService.getUserById(userId);
+    System.out.println(databaseUser);
+    assertEquals(databaseUser, user);
+  }
+
+  @Test
+  void getAllStatisticsByUserId() {
+  }
+}
