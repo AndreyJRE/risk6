bba571ac48f0f637baa1da997cc4c8d488806e17
Finn Sommer <fisommer@mail.uni-mannheim.de>
2023-03-09 18:29:08 +0100

    #18 Update of Log-In View

--- src/main/java/com/unima/risk6/gui/controllers/LoginViewController.java
+++ src/main/java/com/unima/risk6/gui/controllers/LoginViewController.java
@@ -1,12 +1,14 @@
 package com.unima.risk6.gui.controllers;
 
 
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 public class LoginViewController {
 
@@ -23,6 +25,14 @@ public class LoginViewController {
   private void handleLoginButton() {
     String username = usernameField.getText();
     String password = passwordField.getText();
+
+
+    // Change the style of the button to simulate a press
+    loginButton.setStyle("-fx-background-color: linear-gradient(#FFA07A, #FFDAB9); -fx-text-fill: #FFFFFF; -fx-background-radius: 20; -fx-border-radius: 20; -fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.14), 10, 0, 0, 0);");
+    // Change it back after 500 milliseconds
+    PauseTransition pause = new PauseTransition(Duration.millis(500));
+    pause.setOnFinished(e -> loginButton.setStyle("-fx-background-color: linear-gradient(#FFDAB9, #FFA07A); -fx-text-fill: #FFFFFF; -fx-background-radius: 20; -fx-border-radius: 20; -fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.14), 10, 0, 0, 0);"));
+    pause.play();
 
     if (isValidCredentials(username, password)) {
       System.out.println("Login successful!");
@@ -41,7 +51,7 @@ public class LoginViewController {
   @FXML
   private void handleMouseEntered(MouseEvent event) {
     Label label = (Label) event.getSource();
-    label.setStyle("-fx-font-family: 'Segoe UI', sans-serif; -fx-font-size: 12px; -fx-text-fill: #007FFF; -fx-underline: true;");
+    label.setStyle("-fx-font-family: 'Segoe UI', sans-serif; -fx-font-size: 12px; -fx-text-fill: #007FFF; -fx-underline: true; -fx-cursor: hand");
   }
 
   @FXML
