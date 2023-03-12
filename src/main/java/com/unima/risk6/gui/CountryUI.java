59e13ac209d6255dcaa250f48c135344ae4c57e6
Max Meider <mmeider@mail.uni-mannheim.de>
2023-03-12 20:03:46 +0100

    #22 Preparation of classes for GameScene

---
+++ src/main/java/com/unima/risk6/gui/CountryUI.java
@@ -0,0 +1,14 @@
+package com.unima.risk6.gui;
+
+import com.unima.risk6.game.models.Country;
+import javafx.scene.layout.Pane;
+
+public class CountryUI extends Pane {
+
+  private Country country;
+
+  public CountryUI(Country country) {
+    super();
+    this.country = country;
+  }
+}
---
+++ src/main/java/com/unima/risk6/gui/PlayerUI.java
@@ -0,0 +1,15 @@
+package com.unima.risk6.gui;
+
+import com.unima.risk6.game.models.Player;
+import javafx.scene.layout.Pane;
+
+public class PlayerUI extends Pane {
+
+  private Player player;
+
+  public PlayerUI(Player player) {
+    super();
+    this.player = player;
+  }
+
+}
