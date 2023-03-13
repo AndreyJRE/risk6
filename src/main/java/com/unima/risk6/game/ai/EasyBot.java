ed4af9135cb10a78f4427f9716bc1d319279a115
Eliya Ameri <eameri@mail.uni-mannheim.de>
2023-03-13 16:30:38 +0100

    #23 fuller implementation of the easy bot + javadocs

--- src/main/java/com/unima/risk6/game/ai/EasyBot.java
+++ src/main/java/com/unima/risk6/game/ai/EasyBot.java
@@ -12,7 +12,9 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
-/***
+/**
+ * The Easy difficulty bot
+ *
  * @author eameri
  */
 public class EasyBot extends Player implements AiBot {
@@ -23,56 +25,106 @@ public class EasyBot extends Player implements AiBot {
     rng = new Random();
   }
 
+  /**
+   * makes a series of moves depending on which phase the game is in
+   *
+   * @param gameState the current state of the game
+   * @author eameri
+   */
   @Override
   public void makeMove(GameState gameState) { // later: separate based off of round
     if (this.numberOfCountries() == 0) { // unable to make a move if bot is out of the game.
       return;
     }
-    // need a new method: skipPhase() to signal that you are not making a move
     this.createAllReinforcements();
     this.createAllAttacks();
-    this.createFortify(); // maybe add possibility of not fortifying?
+    this.nextPhase();
+    this.createFortify();
+    this.nextPhase();
   }
 
+  /**
+   * Randomly picks countries to attack. The bot will continue to attack the same country until it
+   * loses a fight
+   *
+   * @author eameri
+   */
   public void createAllAttacks() {
     Map<Country, List<Country>> decisions = this.getAllAttackableCountryPairs();
     double attackProbability = 0.8;
     while (rng.nextDouble() < attackProbability) {
       Country[] toAttack = this.getRandomCountryPair(decisions);
-      Attack moveToMake = new Attack(toAttack[0], toAttack[1], 0);
-      do {
-        this.sendAttack(moveToMake);
-        // later calculate attackingTroops and possible changes needed
-        // if attacking again
-      } while (moveToMake.battleWon());
+      Attack attack = createAttack(toAttack);
+      while (attackAgain(attack)) {
+        attack = createAttack(toAttack);
+      }
       attackProbability *= 0.6;
     }
   }
 
+  /**
+   * Creates and sends an Attack object to the server.
+   *
+   * @param toAttack A pair consisting of the two countries attacking and defending
+   * @return the attack object which was sent
+   * @author eameri
+   */
+  public Attack createAttack(Country[] toAttack) {
+    int availableTroops = toAttack[0].getTroops();
+    int attackingTroops = rng.nextInt(1, Math.min(4, availableTroops));
+    Attack moveToMake = new Attack(toAttack[0], toAttack[1], attackingTroops);
+    this.sendAttack(moveToMake);
+    return moveToMake;
+  }
+
+  /**
+   * Randomly picks two countries for the fortify move with a random amount of troops. Low
+   * possibility of choosing not to fortify.
+   *
+   * @author eameri
+   */
   public void createFortify() {
-    // decide if fortify should even be made
+    if (rng.nextDouble() < 0.25) {
+      return;
+    }
     Map<Country, List<Country>> decisions = this.getAllValidFortifies();
     Country[] toFortify = this.getRandomCountryPair(decisions);
     int troopsToMove = rng.nextInt(1, toFortify[0].getTroops());
     this.sendFortify(new Fortify(toFortify[0], toFortify[1], troopsToMove));
   }
 
+  /**
+   * Picks a random amount of troops to send to a random country until there are no more troops to
+   * be added
+   *
+   * @author eameri
+   */
   public void createAllReinforcements() {
-    List<Reinforce> reinforcements = new ArrayList<>();
-    int troopsAvailable = 999; // temp number until we get a variable for available troops
-    while (troopsAvailable > 0) { // implement territory card rules (forced reinforcements
-      // first)
-      int troopsSent = rng.nextInt(1, troopsAvailable);
+    while (this.getReinforcementTroops() > 0) {
+      int troopsSent = rng.nextInt(1, this.getReinforcementTroops());
       this.createReinforce(troopsSent);
-      troopsAvailable -= troopsSent;
+      this.setReinforcementTroops(this.getReinforcementTroops() - troopsSent);
     }
   }
 
+  /**
+   * Creates a Reinforce object and sends it to the server
+   *
+   * @param troops The amount of troops to send to a country
+   * @author eameri
+   */
   public void createReinforce(int troops) {
     Country toReinforce = getRandomCountryFromSet(this.getCountries());
     this.sendReinforce(new Reinforce(toReinforce, troops));
   }
 
+  /**
+   * Randomly pick a country from a set of countries
+   *
+   * @param countrySet a set of countries
+   * @return A randomly chosen country
+   * @author eameri
+   */
   public Country getRandomCountryFromSet(Set<Country> countrySet) {
     int stopIndex = rng.nextInt(this.numberOfCountries());
     int counter = 0;
@@ -85,6 +137,11 @@ public class EasyBot extends Player implements AiBot {
     return null; // will never happen, because we are guaranteed to always have at least one country
   }
 
+  /**
+   * @param decision A mapping of one country to all countries to which a move is allowed
+   * @return An array of 2 Countries (0. from, 1. to)
+   * @author eameri
+   */
   public Country[] getRandomCountryPair(Map<Country, List<Country>> decision) {
     Country[] countryPair = new Country[2];
     List<Country> keyList = new ArrayList<>(decision.keySet());
@@ -93,5 +150,20 @@ public class EasyBot extends Player implements AiBot {
     countryPair[1] = destinationCountries.get(rng.nextInt(destinationCountries.size()));
 
     return countryPair;
+  }
+
+  /**
+   * Lets the bot decide if it should attack the same country again
+   *
+   * @param attack The attack object to be evaluated
+   * @return The roll was won, there are still troops available and the country hasn't been taken
+   * over yet
+   * @author eameri
+   */
+  public boolean attackAgain(Attack attack) {
+    int troopsUsed = attack.getTroopNumber();
+    int troopsLost = attack.getaLosses();
+    int troopsAvailable = attack.getToAttack().getTroops(); // contains number after attack
+    return troopsLost < troopsUsed && troopsAvailable > 1 && !attack.battleWon();
   }
 }
