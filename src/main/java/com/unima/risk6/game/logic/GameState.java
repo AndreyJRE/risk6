abf33490839f2d735d6d0483e0f1db3270d47f65
Weng Phung <wphung@mail.uni-mannheim.de>
2023-03-13 14:50:16 +0100

    #26 Updated GameState class

--- src/main/java/com/unima/risk6/game/logic/GameState.java
+++ src/main/java/com/unima/risk6/game/logic/GameState.java
@@ -5,26 +5,54 @@ import com.unima.risk6.game.models.Country;
 import com.unima.risk6.game.models.Player;
 import com.unima.risk6.game.models.enums.ContinentName;
 import com.unima.risk6.game.models.enums.CountryName;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class GameState {
 
-  private Player[] allPlayer;
-  private HashMap<CountryName, Country> countries;
-  private HashMap<ContinentName, Continent> continents;
+  private Queue<Player> allPlayer;
+  private Set<Country> countries;
+  private Set<Continent> continents;
   private Player currentPlayer;
+  private Dice dice;
+
+  public GameState() {
+    countries = new HashSet<Country>();
+    continents = new HashSet<Continent>();
+    dice= new Dice();
+  }
 
 
-  public void initCountries() {
-    Stream.of(CountryName.values())
-        .forEach((n) -> countries.put(n, new Country(n)));
+  public Set<Country> getCountries() {
+    return countries;
+  }
+
+  public void setPlayerTurn(){
+
+  }
+  public void nextTurn(){
+    currentPlayer = allPlayer.poll();
+  }
+  public void endTurn(){
 
   }
 
-  public void initContinents() {
+  public Player getCurrentPlayer() {
+    return currentPlayer;
+  }
 
+  public Set<Continent> getContinents() {
+    return continents;
+  }
+
+  public void setCurrentPlayer(Player currentPlayer) {
+    this.currentPlayer = currentPlayer;
   }
 }
