fa98662064a15fd5976678101bbbfb0f5bd509ec
Weng Phung <wphung@mail.uni-mannheim.de>
2023-03-16 16:24:37 +0100

    #16 Implemented Enum for GamePhases

---
+++ src/main/java/com/unima/risk6/game/models/enums/GamePhase.java
@@ -0,0 +1,8 @@
+package com.unima.risk6.game.models.enums;
+/**
+ * Enumeration of the possible phases in a game of Risk.
+ * @author wphung
+ */
+public enum GamePhase {
+  REINFORCEMENTPHASE, ATTACKPHASE, FORTIFYPHASE, NOTACTIVE, CLAIMPHASE
+}
