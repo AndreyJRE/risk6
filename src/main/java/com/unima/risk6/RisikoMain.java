798d41da7e9a3497e52a5ec8354bba8edd816d44
Andrey Stoyanov <astoyano@mail.uni-mannheim.de>
2023-03-16 18:49:31 +0100

    #18 Remove system out in constructor and resolve problem with thread in RisikoMain

--- src/main/java/com/unima/risk6/RisikoMain.java
+++ src/main/java/com/unima/risk6/RisikoMain.java
@@ -2,47 +2,41 @@ package com.unima.risk6;
 
 import com.unima.risk6.database.configurations.DatabaseConfiguration;
 import com.unima.risk6.gui.scenes.LogInScreen;
-import com.unima.risk6.gui.scenes.SceneConfiguration;
-import com.unima.risk6.gui.controllers.SceneController;
-import com.unima.risk6.gui.controllers.enums.SceneName;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.io.IOException;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.application.Preloader;
+import javafx.stage.Stage;
 
 public class RisikoMain extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
     /**
-    FXMLLoader fxmlLoader = new FXMLLoader(RisikoMain.class.getResource("fxml/CreateAccount.fxml"));
-    SceneConfiguration.startSceneControllerConfiguration(stage);
-    SceneController sceneController = SceneConfiguration.getSceneController();
-    Scene scene = new Scene(fxmlLoader.load());
+     FXMLLoader fxmlLoader = new FXMLLoader(RisikoMain.class.getResource("fxml/CreateAccount.fxml"));
+     SceneConfiguration.startSceneControllerConfiguration(stage);
+     SceneController sceneController = SceneConfiguration.getSceneController();
+     Scene scene = new Scene(fxmlLoader.load());
      */
     LogInScreen loginScreen = new LogInScreen(stage); // Create instance of the LogInScreen
     stage.setTitle("RISK");
     /**
-    sceneController.addScene(SceneName.LOGIN_SCREEN, scene);
-    sceneController.activate(SceneName.LOGIN_SCREEN);
-    */
+     sceneController.addScene(SceneName.LOGIN_SCREEN, scene);
+     sceneController.activate(SceneName.LOGIN_SCREEN);
+     */
     stage.show();
   }
 
   @Override
   public void init() throws Exception {
-    Thread dbThread = new Thread(() -> {
-      DatabaseConfiguration.startDatabaseConfiguration();
-    });
-    dbThread.start();
+
+    Platform.runLater(() -> DatabaseConfiguration.startDatabaseConfiguration());
 
     // updating of progress bar -> currently disabled for UI testing
-    //for (int i = 0; i < 100000; i++) {
-    //  double progress = (100 * i) / 100000;
-    //  notifyPreloader(new Preloader.ProgressNotification(progress));
-    //}
+    for (int i = 0; i < 100000; i++) {
+      double progress = (100 * i) / 100000;
+      notifyPreloader(new Preloader.ProgressNotification(progress));
+    }
   }
 
   public static void main(String[] args) {
