3f670eb02ea141fd34044ccef5ababd273cd3b65
Max Meider <mmeider@mail.uni-mannheim.de>
2023-04-04 23:01:44 +0200

    #22 Made Map responsive

--- src/main/java/com/unima/risk6/gui/scenes/GameScene.java
+++ src/main/java/com/unima/risk6/gui/scenes/GameScene.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -19,6 +20,10 @@ public class GameScene extends Scene {
 
   private Set<CountryUI> countriesUI;
 
+  private double originalScreenWidth;
+
+  private double originalScreenHeight;
+
   public GameScene(Parent parent,
       int width,
       int height,
@@ -27,36 +32,59 @@ public class GameScene extends Scene {
 //      Stack<CardUI> cardUIS
       Set<CountryUI> countriesUI) {
     super(new Pane(), width, height);
+    this.originalScreenWidth = width;
+    this.originalScreenHeight = height;
 //    this.gameState = gameState;
 //    this.playerUIS = playerUIS;
 //    this.cardUIS = cardUIS;
     this.countriesUI = countriesUI;
 
-    widthProperty().addListener((obs, oldVal, newVal) -> {
-
-      double oldCenterX = oldVal.doubleValue() / 2.0;
-      double newCenterX = newVal.doubleValue() / 2.0;
-      double deltaX = newCenterX - oldCenterX;
-
-      for (CountryUI country : countriesUI) {
-        country.setLayoutX(country.getLayoutX() + deltaX);
-      }
-    });
-    heightProperty().addListener((obs, oldVal, newVal) -> {
-
-      double oldCenterY = oldVal.doubleValue() / 2.0;
-      double newCenterY = newVal.doubleValue() / 2.0;
-      double deltaY = newCenterY - oldCenterY;
-
-      for (CountryUI country : countriesUI) {
-        country.setLayoutY(country.getLayoutY() + deltaY);
-      }
-    });
+    Group countriesGroup = new Group();
+    countriesGroup.getChildren().addAll(countriesUI);
 
     Pane pane = (Pane) getRoot();
-    for (CountryUI country : countriesUI) {
-      pane.getChildren().add(country);
-    }
+    pane.getChildren().add(countriesGroup);
 
+    double widthRatio = getWidth() / originalScreenWidth;
+    double heightRatio = getHeight() / originalScreenHeight;
+    double initialScale = Math.min(widthRatio, heightRatio);
+
+    countriesGroup.setScaleX(initialScale);
+    countriesGroup.setScaleY(initialScale);
+
+    widthProperty().addListener((obs, oldVal, newVal) -> {
+      double widthScale = newVal.doubleValue() / originalScreenWidth;
+      double heightScale = getHeight() / originalScreenHeight;
+      double scale = Math.min(widthScale, heightScale);
+
+      countriesGroup.setScaleX(scale);
+      countriesGroup.setScaleY(scale);
+
+      double newCenterX = newVal.doubleValue() / 2.0;
+      double oldCenterX = oldVal.doubleValue() / 2.0;
+      double deltaX = newCenterX - oldCenterX;
+      countriesGroup.setTranslateX(countriesGroup.getTranslateX() + deltaX);
+    });
+
+    heightProperty().addListener((obs, oldVal, newVal) -> {
+      double widthScale = getWidth() / originalScreenWidth;
+      double heightScale = newVal.doubleValue() / originalScreenHeight;
+      double scale = Math.min(widthScale, heightScale);
+
+      countriesGroup.setScaleX(scale);
+      countriesGroup.setScaleY(scale);
+
+      double newCenterY = newVal.doubleValue() / 2.0;
+      double oldCenterY = oldVal.doubleValue() / 2.0;
+      double deltaY = newCenterY - oldCenterY;
+      countriesGroup.setTranslateY(countriesGroup.getTranslateY() + deltaY);
+    });
+
+    javafx.geometry.Bounds totalBounds = countriesGroup.getBoundsInParent();
+    countriesGroup.setTranslateX((getWidth() - totalBounds.getWidth() * initialScale) / 2.0
+        - totalBounds.getMinX() * initialScale);
+    countriesGroup.setTranslateY((getHeight() - totalBounds.getHeight() * initialScale) / 2.0
+        - totalBounds.getMinY() * initialScale);
   }
 }
+
