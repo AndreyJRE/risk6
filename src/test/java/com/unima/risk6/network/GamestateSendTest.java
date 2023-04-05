1f59f79398bd975a78a873d95843689a073e7761
Jakob Ferch <jferch@mail.uni-mannheim.de>
2023-04-05 22:56:18 +0200

    #20 Added a test to test gamestate serialization and sending to the server

---
+++ src/test/java/com/unima/risk6/network/GamestateSendTest.java
@@ -0,0 +1,50 @@
+package com.unima.risk6.network;
+
+
+import com.unima.risk6.game.ai.AiBot;
+import com.unima.risk6.game.ai.bots.EasyBot;
+import com.unima.risk6.game.configurations.GameConfiguration;
+import com.unima.risk6.game.logic.GameController;
+import com.unima.risk6.game.logic.GameState;
+import com.unima.risk6.network.message.StandardMessage;
+import com.unima.risk6.network.serialization.Deserializer;
+import com.unima.risk6.network.serialization.Serializer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Tests for gamestate sending
+ *
+ * @author jferch
+ */
+
+public class GamestateSendTest {
+    private static GameController gamecontroller;
+    private static GameState gamestate;
+    @BeforeAll
+    static void setUp() {
+
+        try {
+            ArrayList<String> users =  new ArrayList<String>(Arrays.asList("Andrey","Max","Fung"));
+            ArrayList<AiBot> bots = new ArrayList<AiBot>(Arrays.asList(new EasyBot(),new EasyBot()));
+            gamestate = GameConfiguration.configureGame(users, bots);
+            gamecontroller = new GameController(gamestate);
+        } catch (Exception e) {
+        }
+
+    }
+
+
+    @Test
+    void testSerializationWithStatus() {
+        //System.out.println(gamestate);
+        System.out.println(Serializer.serialize(new StandardMessage(gamestate, 200)));
+        assertEquals("{\"statusCode\":200,\"content\":\"tetest\"}", Serializer.serialize(new StandardMessage("tetest", 200)));
+    }
+}
