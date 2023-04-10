d1463e52ab8d227563dbf88a367ee3a87a94cc7a
Eliya Ameri <eameri@mail.uni-mannheim.de>
2023-04-10 23:32:03 +0200

    #12 empty methods to support interface

--- src/main/java/com/unima/risk6/game/ai/bots/HardBot.java
+++ src/main/java/com/unima/risk6/game/ai/bots/HardBot.java
@@ -1,13 +1,23 @@
 package com.unima.risk6.game.ai.bots;
 
 import com.unima.risk6.game.ai.AiBot;
-import com.unima.risk6.game.logic.GameState;
 import com.unima.risk6.game.models.Player;
 
 public class HardBot extends Player implements AiBot {
 
+  /**
+   * A method for a bot to make moves for all 3 phases of the game
+   */
   @Override
-  public void makeMove(GameState gameState) {
+  public void makeMove() {
+
+  }
+
+  /**
+   * A method for a bot to claim a single country during the CLAIM PHASE Game State.
+   */
+  @Override
+  public void claimCountry() {
 
   }
 }
--- src/main/java/com/unima/risk6/game/ai/tutorial/TutorialBot.java
+++ src/main/java/com/unima/risk6/game/ai/tutorial/TutorialBot.java
@@ -1,7 +1,6 @@
 package com.unima.risk6.game.ai.tutorial;
 
 import com.unima.risk6.game.ai.AiBot;
-import com.unima.risk6.game.logic.GameState;
 
 /**
  * The bot used in tutorial mode - all the moves it makes are deterministic
@@ -9,8 +8,19 @@ import com.unima.risk6.game.logic.GameState;
  */
 public class TutorialBot implements AiBot {
 
+  /**
+   * A method for a bot to make moves for all 3 phases of the game
+   */
   @Override
-  public void makeMove(GameState gameState) {
+  public void makeMove() {
+
+  }
+
+  /**
+   * A method for a bot to claim a single country during the CLAIM PHASE Game State.
+   */
+  @Override
+  public void claimCountry() {
 
   }
 }
