2648218ede12d5ce85dfc957440cd1e03b300c4a
Eliya Ameri <eameri@mail.uni-mannheim.de>
2023-04-23 14:58:34 +0200

    #31 minor update to tutorialbot before beginning implementation

--- src/main/java/com/unima/risk6/game/ai/tutorial/TutorialBot.java
+++ src/main/java/com/unima/risk6/game/ai/tutorial/TutorialBot.java
@@ -3,12 +3,22 @@ package com.unima.risk6.game.ai.tutorial;
 import com.unima.risk6.game.ai.AiBot;
 import com.unima.risk6.game.ai.models.MoveTriplet;
 import com.unima.risk6.game.logic.Reinforce;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 /**
  * The bot used in tutorial mode - all the moves it makes are deterministic
  * @author eameri
  */
 public class TutorialBot implements AiBot {
+  private final Queue<Reinforce> deterministicClaims;
+  private final Queue<MoveTriplet> deterministicMoves;
+
+  public TutorialBot() {
+    this.deterministicClaims = this.createAllClaims();
+    this.deterministicMoves = this.createAllMoves();
+  }
 
   /**
    * A method for a bot to make moves for all 3 phases of the game
@@ -24,6 +34,14 @@ public class TutorialBot implements AiBot {
    */
   @Override
   public Reinforce claimCountry() {
+    return null;
+  }
+
+  private Queue<MoveTriplet> createAllMoves() {
+    return null;
+  }
+
+  private Queue<Reinforce> createAllClaims() {
     return null;
   }
 }
