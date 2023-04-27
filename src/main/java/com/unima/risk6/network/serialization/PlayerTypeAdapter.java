b0d9be27e9ad74e61be94e28eb12083571d012b0
Jakob Ferch <jferch@mail.uni-mannheim.de>
2023-04-27 17:07:16 +0200

    #45 Differentiation between bots and players

--- src/main/java/com/unima/risk6/network/serialization/PlayerTypeAdapter.java
+++ src/main/java/com/unima/risk6/network/serialization/PlayerTypeAdapter.java
@@ -1,6 +1,11 @@
 package com.unima.risk6.network.serialization;
 
 import com.google.gson.*;
+import com.unima.risk6.database.configurations.DatabaseConfiguration;
+import com.unima.risk6.game.ai.bots.EasyBot;
+import com.unima.risk6.game.ai.bots.HardBot;
+import com.unima.risk6.game.ai.bots.MediumBot;
+import com.unima.risk6.game.ai.bots.MonteCarloBot;
 import com.unima.risk6.game.models.Continent;
 import com.unima.risk6.game.models.Country;
 import com.unima.risk6.game.models.Hand;
@@ -9,15 +14,18 @@ import com.unima.risk6.game.models.enums.GamePhase;
 import java.lang.reflect.Type;
 import java.util.HashSet;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PlayerTypeAdapter implements JsonSerializer<Player>, JsonDeserializer<Player>{
+
+  private final static Logger LOGGER = LoggerFactory.getLogger(PlayerTypeAdapter.class);
 
   @Override
   public JsonElement serialize(Player player, Type typeOfSrc, JsonSerializationContext context) {
     if (player == null) {
       return null;
     }
-
     JsonObject jsonObject = new JsonObject();
 
     jsonObject.add("hand", context.serialize(player.getHand(), Hand.class));
@@ -42,7 +50,7 @@ public class PlayerTypeAdapter implements JsonSerializer<Player>, JsonDeserializ
     jsonObject.add("currentPhase", context.serialize(player.getCurrentPhase(), GamePhase.class));
     jsonObject.addProperty("deployableTroops", player.getDeployableTroops());
     jsonObject.addProperty("initialTroops", player.getInitialTroops());
-
+    jsonObject.addProperty("type", player.getClass().getSimpleName());
 
 
     return jsonObject;
@@ -50,6 +58,9 @@ public class PlayerTypeAdapter implements JsonSerializer<Player>, JsonDeserializ
   @Override
   public Player deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
     JsonObject jsonObject = json.getAsJsonObject();
+
+    Player player;
+
 
     Hand hand = context.deserialize(jsonObject.get("hand"), Hand.class);
 
@@ -70,7 +81,24 @@ public class PlayerTypeAdapter implements JsonSerializer<Player>, JsonDeserializ
     int deployableTroops = jsonObject.get("deployableTroops").getAsInt();
     int initialTroops = jsonObject.get("initialTroops").getAsInt();
 
-    Player player = new Player(user);
+    switch(jsonObject.get("type").getAsString()){
+      case "Player":
+        player = new Player(user);
+        break;
+      case "EasyBot":
+        player = new EasyBot();
+        break;
+      case "MediumBot":
+        player = new MediumBot();
+        break;
+      case "HardBot":
+        player = new HardBot();
+        break;
+      default:
+        player = new Player();
+        LOGGER.debug("PlayerType should be Player, EasyBot, MediumBot or Hardbot");
+        break;
+    }
     //TODO
     /*player.setHand(hand);
     player.setCountries(countries);
