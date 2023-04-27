ff11a50b226f7340052d1b21d4a7433ff2867013
Jakob Ferch <jferch@mail.uni-mannheim.de>
2023-04-27 17:04:44 +0200

    #45 Added Custom Serialization for Card

---
+++ src/main/java/com/unima/risk6/network/serialization/CardTypeAdapter.java
@@ -0,0 +1,45 @@
+package com.unima.risk6.network.serialization;
+
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.unima.risk6.game.models.Card;
+import com.unima.risk6.game.models.enums.CardSymbol;
+import com.unima.risk6.game.models.enums.CountryName;
+
+import java.lang.reflect.Type;
+
+public class CardTypeAdapter implements JsonDeserializer<Card>, JsonSerializer<Card> {
+
+  @Override
+  public JsonElement serialize(Card src, Type typeOfSrc, JsonSerializationContext context) {
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.add("cardSymbol", context.serialize(src.getCardSymbol(), CardSymbol.class));
+    jsonObject.addProperty("hasCountry", src.isHasCountry());
+
+    if (src.isHasCountry()) {
+      jsonObject.add("country", context.serialize(src.getCountry(), CountryName.class));
+    }
+
+    return jsonObject;
+  }
+
+  @Override
+  public Card deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+    JsonObject jsonObject = json.getAsJsonObject();
+    CardSymbol cardSymbol = context.deserialize(jsonObject.get("cardSymbol"), CardSymbol.class);
+    boolean hasCountry = jsonObject.get("hasCountry").getAsBoolean();
+
+    if (hasCountry) {
+      CountryName country = context.deserialize(jsonObject.get("country"), CountryName.class);
+      return new Card(cardSymbol, country);
+    } else {
+      return new Card(cardSymbol);
+    }
+  }
+}
