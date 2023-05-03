9025aa3578c6fc15b18db323b05fe3603a9effee
Weng Phung <wphung@mail.uni-mannheim.de>
2023-05-03 16:16:13 +0200

    #35 Updated MoveProcessor

--- src/main/java/com/unima/risk6/network/server/MoveProcessor.java
+++ src/main/java/com/unima/risk6/network/server/MoveProcessor.java
@@ -1,6 +1,9 @@
 package com.unima.risk6.network.server;
 
+import static com.unima.risk6.game.models.enums.GamePhase.ATTACK_PHASE;
 import static com.unima.risk6.game.models.enums.GamePhase.CLAIM_PHASE;
+import static com.unima.risk6.game.models.enums.GamePhase.FORTIFY_PHASE;
+import static com.unima.risk6.game.models.enums.GamePhase.REINFORCEMENT_PHASE;
 
 import com.unima.risk6.game.logic.Attack;
 import com.unima.risk6.game.logic.EndPhase;
@@ -15,7 +18,9 @@ import com.unima.risk6.game.models.Card;
 import com.unima.risk6.game.models.Country;
 import com.unima.risk6.game.models.Player;
 import com.unima.risk6.game.models.Statistic;
+import com.unima.risk6.game.models.enums.CountryName;
 import com.unima.risk6.game.models.enums.GamePhase;
+import java.util.ArrayList;
 import java.util.Set;
 
 //TODO @FUNG zurückklauen
@@ -31,104 +36,125 @@ public class MoveProcessor {
     this.gameController = gameController;
     this.deckController = deckController;
     this.playerController = playerController;
-
   }
 
   public void processFortify(Fortify fortify) {
     //TODO Unterscheiden ob in Fortify Phase
-    gameController.addLastMove(fortify);
+
     //Validate if the fortify is legal
-    if (fortify.getTroopsToMove() < fortify.getOutgoing().getTroops() && fortify.getOutgoing()
-        .getPlayer().equals(fortify.getIncoming().getPlayer())) {
-      fortify.getIncoming().changeTroops(fortify.getTroopsToMove());
-      fortify.getOutgoing().changeTroops(-fortify.getTroopsToMove());
-    }
-    Player currentPlayer = gameController.getCurrentPlayer();
-    if (currentPlayer.isHasConquered()) {
-      this.drawCard();
-      currentPlayer.setHasConquered(false);
-    }
-    if (currentPlayer.getCurrentPhase().equals(GamePhase.FORTIFY_PHASE)) {
+    Country incoming = getCountryByCountryName(fortify.getIncoming().getCountryName());
 
-    }
+    Country outgoing = getCountryByCountryName(fortify.getOutgoing().getCountryName());
+    if (fortify.getTroopsToMove() < outgoing.getTroops() && outgoing
+        .getPlayer().equals(incoming.getPlayer()) && incoming
+        .getAdjacentCountries().contains(outgoing)) {
+      incoming.changeTroops(fortify.getTroopsToMove());
+      outgoing.changeTroops(-fortify.getTroopsToMove());
+      gameController.addLastMove(fortify);
 
+      Player currentPlayer = gameController.getCurrentPlayer();
+      if (currentPlayer.getCurrentPhase().equals(GamePhase.FORTIFY_PHASE)) {
+        processEndPhase(new EndPhase(FORTIFY_PHASE));
+      }
+    } else {
+      //TODO throw invalid move exception.
+    }
 
   }
 
   public void processReinforce(Reinforce reinforce) {
     //TODO Schick gameState an Client
-    gameController.addLastMove(reinforce);
     Player currentPlayer = gameController.getCurrentPlayer();
-    //Should process a Reinforce during the ClaimPhase differently
-    Country countryToReinforce = reinforce.getCountry();
-    if (currentPlayer.getCurrentPhase().equals(CLAIM_PHASE) && !countryToReinforce.hasPlayer()) {
-      if (!countryToReinforce.hasPlayer()) {
-        countryToReinforce.setPlayer(currentPlayer);
-        playerController.addCountry(countryToReinforce);
-      }
-      //TODO Schick gameState an Client
-      this.processEndPhase(new EndPhase(CLAIM_PHASE));
-      currentPlayer.setInitialTroops(currentPlayer.getInitialTroops() - 1);
-      countryToReinforce.setTroops(countryToReinforce.getTroops() + 1);
 
-    } else {
-      countryToReinforce.setTroops(countryToReinforce.getTroops() + reinforce.getToAdd());
-      currentPlayer.setDeployableTroops(currentPlayer.getDeployableTroops() - reinforce.getToAdd());
+    if (currentPlayer.getCurrentPhase().equals(REINFORCEMENT_PHASE)
+        || currentPlayer.getCurrentPhase().equals(CLAIM_PHASE)) {
+      gameController.addLastMove(reinforce);
 
-      if (currentPlayer.getDeployableTroops() == 0) {
+      //Should process a Reinforce during the ClaimPhase differently
+      Country countryToReinforce = getCountryByCountryName(reinforce.getCountry().getCountryName());
+      if (currentPlayer.getCurrentPhase().equals(CLAIM_PHASE)) {
+        if (!countryToReinforce.hasPlayer()) {
+          countryToReinforce.setPlayer(currentPlayer);
+          playerController.addCountry(countryToReinforce);
+        }
         //TODO Schick gameState an Client
-        processEndPhase(new EndPhase(GamePhase.REINFORCEMENT_PHASE));
+        currentPlayer.setInitialTroops(currentPlayer.getInitialTroops() - 1);
+        countryToReinforce.setTroops(countryToReinforce.getTroops() + 1);
+        this.processEndPhase(new EndPhase(CLAIM_PHASE));
 
+
+      } else {
+        countryToReinforce.setTroops(countryToReinforce.getTroops() + reinforce.getToAdd());
+        currentPlayer.setDeployableTroops(
+            currentPlayer.getDeployableTroops() - reinforce.getToAdd());
+        if (currentPlayer.getDeployableTroops() == 0) {
+          //TODO Schick gameState an Client
+          processEndPhase(new EndPhase(GamePhase.REINFORCEMENT_PHASE));
+        }
       }
+    } else {
+      //TODO Throw invalid move Exception
     }
   }
 
-
   public void processAttack(Attack attack) {
+    Country attackingCountry = getCountryByCountryName(
+        attack.getAttackingCountry().getCountryName());
 
-    Country attackingCountry = attack.getAttackingCountry();
-    Country defendingCountry = attack.getDefendingCountry();
+    Country defendingCountry = getCountryByCountryName(
+        attack.getDefendingCountry().getCountryName());
+
     Player attacker = attackingCountry.getPlayer();
     Player defender = defendingCountry.getPlayer();
 
-    attack.calculateLosses();
-    gameController.addLastMove(attack);
-    //TODO Schick gameState an Client
-    attackingCountry.changeTroops(-attack.getAttackerLosses());
-    defendingCountry.changeTroops(-attack.getDefenderLosses());
+    //check if attack is legal.
+    if (attacker.equals(gameController.getCurrentPlayer()) && !attacker.equals(defender)
+        && defendingCountry.getAdjacentCountries().contains(attackingCountry)
+        && attack.getTroopNumber() <= attackingCountry.getTroops() - 1) {
+      attack.calculateLosses();
 
-    //Increase statistics
-    Statistic attackerStatistic = attacker.getStatistic();
-    Statistic defenderStatistic = defender.getStatistic();
-    //Increase statistics for troopsLost
-    attackerStatistic.setTroopsLost(attackerStatistic.getTroopsLost() + attack.getAttackerLosses());
-    defenderStatistic.setTroopsLost(defenderStatistic.getTroopsLost() + attack.getDefenderLosses());
+      gameController.addLastMove(attack);
+      //TODO Schick gameState an Client
+      attackingCountry.changeTroops(-attack.getAttackerLosses());
+      defendingCountry.changeTroops(-attack.getDefenderLosses());
+      //Increase statistics
+      Statistic attackerStatistic = attacker.getStatistic();
+      Statistic defenderStatistic = defender.getStatistic();
+      //Increase statistics for troopsLost
+      attackerStatistic.setTroopsLost(
+          attackerStatistic.getTroopsLost() + attack.getAttackerLosses());
+      defenderStatistic.setTroopsLost(
+          defenderStatistic.getTroopsLost() + attack.getDefenderLosses());
 
-    if (defendingCountry.getTroops() == 0) {
-      gameController.getCurrentPlayer().setHasConquered(true);
-      defendingCountry.setPlayer(attacker);
+      if (attack.getHasConquered()) {
+        gameController.getCurrentPlayer().setHasConquered(true);
+        defendingCountry.setPlayer(attacker);
 
-      playerController.addCountry(defendingCountry);
+        playerController.addCountry(defendingCountry);
+        playerController.setPlayer(defender);
+        playerController.removeCountry(defendingCountry);
+        playerController.setPlayer(attacker);
+
+        //Increase statistic for countriesLost and countriesWon
+        defenderStatistic.setCountriesLost(defenderStatistic.getCountriesLost() + 1);
+        defenderStatistic.setCountriesWon(attackerStatistic.getCountriesWon() + 1);
+
+        //Forced Fortify after attack and takeover
+        Fortify forcedFortify = new Fortify(attackingCountry, defendingCountry,
+            attack.getTroopNumber());
+        processFortify(forcedFortify);
+        //TODO OPTIONAL Fortify NOW Attack has won HOW TO SIGNAL?
+
+      }
       playerController.setPlayer(defender);
-      playerController.removeCountry(defendingCountry);
+      if (playerController.getNumberOfCountries() == 0) {
+        gameController.removeLostPlayer(defender);
+      }
       playerController.setPlayer(attacker);
-
-      //Increase statistic for countriesLost and countriesWon
-      defenderStatistic.setCountriesLost(defenderStatistic.getCountriesLost() + 1);
-      defenderStatistic.setCountriesWon(attackerStatistic.getCountriesWon() + 1);
-
-      //Forced Fortify after attack and takeover
-      Fortify forcedFortify = new Fortify(attackingCountry, defendingCountry,
-          attack.getTroopNumber());
-      processFortify(forcedFortify);
-      //TODO OPTIONAL Fortify (NOW Attack has won
+    } else {
+      //TODO Throw MoveNotValid Exception
 
     }
-    playerController.setPlayer(defender);
-    if (playerController.getNumberOfCountries() == 0) {
-      gameController.removeLostPlayer(defender);
-    }
-    playerController.setPlayer(attacker);
 
   }
 
@@ -141,19 +167,27 @@ public class MoveProcessor {
   }
 
   public void processHandIn(HandIn handIn) {
-    int numberOfHandIn = gameController.getGameState().getNumberOfHandIns();
     HandController handController = playerController.getHandController();
-    if (handController.isExchangeable(handIn.getCards())) {
+    ArrayList<Card> cardsOfPlayer = new ArrayList<>();
+    handController.selectCardsFromCardList(handIn.getCards());
+    //Checks if HandIn is valid.
+    if (gameController.getCurrentPlayer().getCurrentPhase().equals(REINFORCEMENT_PHASE)
+        && handController.isExchangeable()) {
+
+      //TODO SEND Process/ gamestate
+      gameController.addLastMove(handIn);
+
       Set<Country> countries = playerController.getPlayer().getCountries();
-      if (!handController.hasCountryBonus(countries).isEmpty()) {
-        //TODO add troops in those countries
-        handController.hasCountryBonus(countries).forEach(n -> {
+      if (!handController.getBonusCountries(countries).isEmpty()) {
+        handController.getBonusCountries(countries).forEach(n -> {
           //TODO SEND process/ GameState
-          this.processReinforce(new Reinforce(n, 2));
+          playerController.changeDeployableTroops(2);
+          System.out.println("yeet");
+          this.processReinforce(new Reinforce(getCountryByCountryName(n), 2));
         });
 
       }
-      handController.exchangeCards();
+      int numberOfHandIn = gameController.getGameState().getNumberOfHandIns();
       int diff = 0;
       if (numberOfHandIn > 5) {
         diff = 15 + 5 * (numberOfHandIn - 6);
@@ -166,12 +200,37 @@ public class MoveProcessor {
       statisticOfCurrentPlayer.setTroopsGained(
           statisticOfCurrentPlayer.getTroopsGained() + diff);
 
+      gameController.getGameState().setNumberOfHandIns(numberOfHandIn + 1);
+      handController.exchangeCards();
+    } else {
+      handController.deselectAllCards();
+      System.out.println("Karte geht nicht");
     }
 
   }
 
   public void processEndPhase(EndPhase endPhase) {
+    Player currentPlayer = gameController.getCurrentPlayer();
+    if (currentPlayer.getHasConquered() && endPhase.getPhaseToEnd().equals(ATTACK_PHASE)) {
+      this.drawCard();
+      currentPlayer.setHasConquered(false);
+    }
+
     gameController.addLastMove(endPhase);
     gameController.nextPhase();
+    playerController.setPlayer(gameController.getCurrentPlayer());
+
   }
+
+
+  Country getCountryByCountryName(CountryName countryName) {
+    final Country[] country = new Country[1];
+    gameController.getGameState().getCountries().stream()
+        .filter(n -> n.getCountryName().equals(countryName))
+        .forEach(n -> country[0] = n);
+    return country[0];
+  }
+
+
 }
+
