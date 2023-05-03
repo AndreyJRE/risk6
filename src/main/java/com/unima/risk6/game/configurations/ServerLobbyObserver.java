693271ef4971f3e6bdc623f60e2245f09e02c853
Andrey Stoyanov <astoyano@mail.uni-mannheim.de>
2023-05-03 22:46:29 +0200

    #48 Preparation for integration of server and game lobby

---
+++ src/main/java/com/unima/risk6/game/configurations/ServerLobbyObserver.java
@@ -0,0 +1,9 @@
+package com.unima.risk6.game.configurations;
+
+import com.unima.risk6.game.models.ServerLobby;
+
+public interface ServerLobbyObserver {
+
+  void updateServerLobby(ServerLobby serverLobby);
+
+}
