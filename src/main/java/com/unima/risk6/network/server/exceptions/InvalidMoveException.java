a357138806a295a98aed618feffd1e8857b2c436
Weng Phung <wphung@mail.uni-mannheim.de>
2023-05-04 16:55:16 +0200

    #30 Added some JavaDoc

--- src/main/java/com/unima/risk6/network/server/exceptions/InvalidMoveException.java
+++ src/main/java/com/unima/risk6/network/server/exceptions/InvalidMoveException.java
@@ -1,6 +1,6 @@
 package com.unima.risk6.network.server.exceptions;
 
-public class InvalidMoveException extends Exception {
+public class InvalidMoveException extends RuntimeException {
 
   public InvalidMoveException(String message) {
     super(message);
