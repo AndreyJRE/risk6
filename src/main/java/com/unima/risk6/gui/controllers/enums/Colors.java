5c26d48814b4f0e26b9a7a8f407dab714c0258ed
Jakob Ferch <jferch@mail.uni-mannheim.de>
2023-05-13 18:23:54 +0200

    #22 Dark country border

--- src/main/java/com/unima/risk6/gui/controllers/enums/Colors.java
+++ src/main/java/com/unima/risk6/gui/controllers/enums/Colors.java
@@ -9,8 +9,9 @@ import javafx.scene.paint.Color;
  */
 public enum Colors {
   COUNTRY_BACKGROUND(Color.rgb(249, 236, 195)), COUNTRY_BACKGROUND_DARKEN(
-      Color.rgb(243, 217, 134)), COUNTRY_STROKE(Color.rgb(254, 251, 244)), WATER(
-      Color.rgb(149, 115, 97)),
+      Color.rgb(243, 217, 134)), //COUNTRY_STROKE(Color.rgb(254, 251, 244)),
+  COUNTRY_STROKE(Color.rgb(0, 7, 45)),
+  WATER(Color.rgb(149, 115, 97)),
   TEXT(Color.rgb(0, 7, 45));
   private final Color color;
 
