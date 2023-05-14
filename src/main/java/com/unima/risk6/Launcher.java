602ce6718ece1cd05f44597c730dd0110fc4a93e
Finn Sommer <fisommer@mail.uni-mannheim.de>
2023-05-14 20:14:54 +0200

    #22 adding Java docs

--- src/main/java/com/unima/risk6/Launcher.java
+++ src/main/java/com/unima/risk6/Launcher.java
@@ -1,5 +1,12 @@
 package com.unima.risk6;
 
+/**
+ * The Launcher class contains the main method which calls the main method of the RisikoMain class.
+ * This class is used to launch the application.
+ *
+ * @author astoyano
+ */
+
 public class Launcher {
 
   public static void main(String[] args) {
