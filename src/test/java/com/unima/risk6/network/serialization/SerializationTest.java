cfa627ef485e6bf9734381f800c6c901fd2338f7
Jakob Ferch <jferch@mail.uni-mannheim.de>
2023-05-14 19:33:02 +0200

    #45 Update SerializationTest

--- src/test/java/com/unima/risk6/network/serialization/SerializationTest.java
+++ src/test/java/com/unima/risk6/network/serialization/SerializationTest.java
@@ -1,5 +1,9 @@
 package com.unima.risk6.network.serialization;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.unima.risk6.game.ai.AiBot;
 import com.unima.risk6.game.ai.bots.EasyBot;
 import com.unima.risk6.game.ai.bots.MediumBot;
@@ -14,15 +18,11 @@ import com.unima.risk6.game.models.Player;
 import com.unima.risk6.game.models.enums.ContinentName;
 import com.unima.risk6.game.models.enums.CountryName;
 import com.unima.risk6.network.message.StandardMessage;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 /**
  * Tests for object serialization
@@ -33,72 +33,61 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SerializationTest {
 
-  @BeforeAll
-  static void setUp() {
-    try {
-
-    } catch (Exception e) {
-    }
-
-  }
-
-
   @Test
   void testSerializationWithStatus() {
     assertEquals("{\"statusCode\":200,\"content\":\"tetest\",\"contentType\":\"DEFAULT\"}",
-        Serializer.serialize(new StandardMessage("tetest", 200)));
+        Serializer.serialize(new StandardMessage<>("tetest", 200)));
   }
 
   @Test
   void testSerializationWithoutStatus() {
     assertEquals("{\"statusCode\":-1,\"content\":\"tetest\",\"contentType\":\"DEFAULT\"}",
-        Serializer.serialize(new StandardMessage("tetest")));
+        Serializer.serialize(new StandardMessage<>("tetest")));
 
   }
 
   @Test
   void testSerializationAndDeserialization() {
-    StandardMessage standard = new StandardMessage("tetest");
+    StandardMessage<String> standard = new StandardMessage<>("tetest");
     String json = Serializer.serialize(standard);
-    StandardMessage s2 = (StandardMessage) Deserializer.deserialize(json);
-    //System.out.println(s2.getStatusCode() + (String)s2.getContent());
-
-    //assertEquals(standard, s2);
+    @SuppressWarnings("unchecked")
+    StandardMessage<String> s2 = (StandardMessage<String>) Deserializer.deserialize(json);
     assertTrue(standard.equals(s2));
 
   }
 
   @Test
   void testEmptyPlayerSerialization() {
-    ArrayList<String> users = new ArrayList<String>(Arrays.asList("Peter"));
-    ArrayList<AiBot> bots = new ArrayList<AiBot>();
+    ArrayList<String> users = new ArrayList<>(List.of("Peter"));
+    ArrayList<AiBot> bots = new ArrayList<>();
     GameState gamestate = GameConfiguration.configureGame(users, bots);
     StandardMessage<GameState> message = new StandardMessage<>(gamestate);
+    @SuppressWarnings("unchecked")
     StandardMessage<GameState> m2 = (StandardMessage<GameState>) Deserializer.deserialize(
         Serializer.serialize(message),
-        GameConfiguration.configureGame(new ArrayList<String>(), new ArrayList<AiBot>()));
+        GameConfiguration.configureGame(new ArrayList<>(), new ArrayList<>()));
 
     Player p1 = gamestate.getCurrentPlayer();
     Player p2 = m2.getContent().getCurrentPlayer();
     System.out.println("P1: " + p1.getUser() + " P2: " + p2.getUser());
-    assertTrue(p1.equals(p2));
+    assertEquals(p1, p2);
 
   }
 
   @Test
   void testPlayerSerializationWithOneCountry() {
-    HashMap<CountryName, Country> countrys = new HashMap<>();
-    ArrayList<String> users = new ArrayList<String>(Arrays.asList("Peter"));
-    ArrayList<AiBot> bots = new ArrayList<AiBot>();
+    ArrayList<String> users = new ArrayList<>(List.of("Peter"));
+    ArrayList<AiBot> bots = new ArrayList<>();
     GameState oldGamestate = GameConfiguration.configureGame(users, bots);
     oldGamestate.getCurrentPlayer().getCountries()
-        .add(oldGamestate.getCountries().stream().findFirst().get());
+        .add(oldGamestate.getCountries().stream().findFirst().orElseThrow());
     StandardMessage<GameState> message = new StandardMessage<>(oldGamestate);
     String json = Serializer.serialize(message);
     System.out.println(json);
 
-    GameState newGamestate = GameConfiguration.configureGame(new ArrayList<String>(),
-        new ArrayList<AiBot>());
+    GameState newGamestate = GameConfiguration.configureGame(new ArrayList<>(),
+        new ArrayList<>());
+    @SuppressWarnings("unchecked")
     StandardMessage<GameState> m2 = (StandardMessage<GameState>) Deserializer.deserialize(json,
         newGamestate);
     Player p1 = oldGamestate.getCurrentPlayer();
@@ -106,33 +95,13 @@ public class SerializationTest {
     Player p2 = g2.getCurrentPlayer();
     System.out.println("P1: " + p1.getUser() + " P2: " + p2.getUser());
     //g2 is the same as newGamestate
-    Country country = g2.getCurrentPlayer().getCountries().stream().findFirst().get();
-    assertTrue(country == g2.getCountries().stream()
-        .filter(x -> x.getCountryName().equals(country.getCountryName())).findFirst().get());
+    Country country = g2.getCurrentPlayer().getCountries().stream().findFirst().orElseThrow();
+    assertSame(country, g2.getCountries().stream()
+        .filter(x -> x.getCountryName().equals(country.getCountryName())).findFirst()
+        .orElseThrow());
 
   }
 
-  /*
-  @Test
-  void testEasyBotSerialization() {
-    System.out.println("EasyBotSrialization");
-    ArrayList<String> users =  new ArrayList<String>(Arrays.asList("Andrey","Max","Fung"));
-    ArrayList<AiBot> bots = new ArrayList<AiBot>();
-    GameState gameState = GameConfiguration.configureGame(users, bots);
-    gameState.setCurrentPhase(GamePhase.CLAIMPHASE);
-    EasyBot bot = new EasyBot(gameState);
-    StandardMessage<Player> s = new StandardMessage<Player>(bot);
-    String json = Serializer.serialize(s);
-    System.out.println(json);
-    //StandardMessage s2 = (StandardMessage) Deserializer.deserialize(json);
-    //System.out.println(s2.getStatusCode() + (String)s2.getContent());
-    StandardMessage message = (StandardMessage) Deserializer.deserialize(json);
-    //TODO
-    assertTrue(bot.equals(message.getContent()));
-
-  }
-
- */
   @Test
   void testAttackSerialization() {
     Country c1 = new Country(CountryName.ALASKA);
@@ -143,8 +112,9 @@ public class SerializationTest {
     Attack a = new Attack(c1, c2, 69);
     StandardMessage<Attack> message = new StandardMessage<>(a);
     System.out.println("Attack Serialization:\n" + Serializer.serialize(message));
-    assertTrue(Serializer.serialize(message).equals(
-        "{\"statusCode\":-1,\"content\":{\"attackingCountry\":{\"countryName\":\"ALASKA\",\"hasPlayer\":false,\"troops\":0,\"adjacentCountries\":[],\"continent\":\"AFRICA\"},\"defendingCountry\":{\"countryName\":\"ICELAND\",\"hasPlayer\":false,\"troops\":0,\"adjacentCountries\":[],\"continent\":\"AFRICA\"},\"troopNumber\":69,\"attackerLosses\":0,\"defenderLosses\":0,\"attackDiceResult\":[],\"defendDiceResult\":[]},\"contentType\":\"ATTACK\"}"));
+    assertEquals(
+        "{\"statusCode\":-1,\"content\":{\"attackingCountry\":\"ALASKA\",\"defendingCountry\":\"ICELAND\",\"troopNumber\":69,\"attackerLosses\":0,\"defenderLosses\":0,\"attackDiceResult\":[],\"defendDiceResult\":[],\"hasConquered\":false},\"contentType\":\"ATTACK\"}",
+        Serializer.serialize(message));
 
   }
 
@@ -158,8 +128,9 @@ public class SerializationTest {
     Fortify a = new Fortify(c1, c2, 69);
     StandardMessage<Fortify> message = new StandardMessage<>(a);
     System.out.println("Fortify Serialization:\n" + Serializer.serialize(message));
-    assertTrue(Serializer.serialize(message).equals(
-        "{\"statusCode\":-1,\"content\":{\"outgoing\":{\"countryName\":\"ALASKA\",\"hasPlayer\":false,\"troops\":0},\"incoming\":{\"countryName\":\"ICELAND\",\"hasPlayer\":false,\"troops\":0},\"troopsToMove\":69},\"contentType\":\"FORTIFY\"}"));
+    assertEquals(
+        "{\"statusCode\":-1,\"content\":{\"outgoing\":\"ALASKA\",\"incoming\":\"ICELAND\",\"troopsToMove\":69},\"contentType\":\"FORTIFY\"}",
+        Serializer.serialize(message));
   }
 
   @Test
@@ -170,14 +141,15 @@ public class SerializationTest {
     Reinforce a = new Reinforce(c1, 69);
     StandardMessage<Reinforce> message = new StandardMessage<>(a);
     System.out.println("Reinforce Serialization:\n" + Serializer.serialize(message));
-    assertTrue(Serializer.serialize(message).equals(
-        "{\"statusCode\":-1,\"content\":{\"country\":{\"countryName\":\"ALASKA\",\"hasPlayer\":false,\"troops\":0,\"adjacentCountries\":[],\"continent\":\"AFRICA\"},\"toAdd\":69},\"contentType\":\"REINFORCE\"}"));
+    assertEquals(
+        "{\"statusCode\":-1,\"content\":{\"country\":\"ALASKA\",\"toAdd\":69},\"contentType\":\"REINFORCE\"}",
+        Serializer.serialize(message));
   }
 
   @Test
   void testInitialGamestateSerialization() {
-    ArrayList<String> users = new ArrayList<String>(Arrays.asList(""));
-    ArrayList<AiBot> bots = new ArrayList<AiBot>(Arrays.asList(new MediumBot(), new EasyBot()));
+    ArrayList<String> users = new ArrayList<>(List.of(""));
+    ArrayList<AiBot> bots = new ArrayList<>(Arrays.asList(new MediumBot(), new EasyBot()));
     GameState gamestate = GameConfiguration.configureGame(users, bots);
     Country c1 = new Country(CountryName.ALASKA);
     c1.setContinent(new Continent(ContinentName.AFRICA));
@@ -195,25 +167,30 @@ public class SerializationTest {
 
     StandardMessage<GameState> message = new StandardMessage<>(gamestate);
 
+    @SuppressWarnings("unchecked")
     StandardMessage<GameState> m2 = (StandardMessage<GameState>) Deserializer.deserialize(
         Serializer.serialize(message),
-        GameConfiguration.configureGame(new ArrayList<String>(), new ArrayList<AiBot>()));
+        GameConfiguration.configureGame(new ArrayList<>(), new ArrayList<>()));
     GameState g2 = m2.getContent();
     String message1 = Serializer.serialize(message);
-    String message2 = Serializer.serialize(new StandardMessage<GameState>(g2));
+    String message2 = Serializer.serialize(new StandardMessage<>(g2));
     System.out.println("GameState Serialization:\n" + message1);
     System.out.println("GameState Serialization:\n" + message2);
-    //Failed wegen Hashcode
-    assertTrue(message1.equals(message2));
+    System.out.println(message1.replaceAll(".hashCode..\\d*?,", ""));
+
+    //Failed ohne regex wegen Hashcode
+    assertEquals(message1.replaceAll(".hashCode..\\d*?,", ""),
+        message2.replaceAll(".hashCode..\\d*?,", ""));
   }
 
   @Test
   void testOrderMessageSerializationTest() {
-    HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+    HashMap<String, Integer> hashMap = new HashMap<>();
     hashMap.put("lel", 1);
     hashMap.put("lol", 2);
-    StandardMessage<HashMap> message = new StandardMessage<HashMap>(hashMap);
+    StandardMessage<HashMap<String, Integer>> message = new StandardMessage<>(hashMap);
     System.out.println(Serializer.serialize(message));
+    @SuppressWarnings("unchecked")
     HashMap<String, Integer> hashMap2 = (HashMap<String, Integer>) Deserializer.deserialize(Serializer.serialize(message)).getContent();
     assertEquals(hashMap.get("lel"), hashMap2.get("lel"));
   }
