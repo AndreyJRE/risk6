dd17e9f5fc94dbb393e63598b5c501a229955f21
Andrey Stoyanov <astoyano@mail.uni-mannheim.de>
2024-11-04 20:21:17 +0100

    Changed version number in splash screen to 1.1

--- src/main/java/com/unima/risk6/RiskPreloader.java
+++ src/main/java/com/unima/risk6/RiskPreloader.java
@@ -12,51 +12,51 @@ import javafx.stage.StageStyle;
 
 
 /**
- * A custom preloader class for our Risk application. This will be shown while our database gets
- * configured and our images and sounds get loaded.
+ * A custom preloader class for our Risk application. This will be shown while our database gets configured and our images and sounds get
+ * loaded.
  */
 public class RiskPreloader extends Preloader {
 
-  private Stage preloaderStage;
-  private Scene scene;
-  private SplashScreenController splashScreenController;
+    private Stage preloaderStage;
+    private Scene scene;
+    private SplashScreenController splashScreenController;
 
-  public RiskPreloader() {
+    public RiskPreloader() {
 
-  }
-
-  @Override
-  public void init() throws Exception {
-    FXMLLoader fxmlLoader = new FXMLLoader(
-        RiskPreloader.class.getResource("fxml/splashScreen.fxml"));
-    scene = new Scene(fxmlLoader.load());
-    splashScreenController = fxmlLoader.getController();
-  }
-
-  @Override
-  public void start(Stage stage) throws Exception {
-    this.preloaderStage = stage;
-    preloaderStage.getIcons().add(new Image(Objects.requireNonNull(RisikoMain.class.getResourceAsStream("pictures/logo.png"))));
-    preloaderStage.setScene(scene);
-    preloaderStage.initStyle(StageStyle.UNDECORATED);
-    preloaderStage.show();
-  }
-
-  @Override
-  public void handleApplicationNotification(Preloader.PreloaderNotification info) {
-    if (info instanceof ProgressNotification i) {
-      splashScreenController.getProgress()
-          .setText("Version 1.0        Loading " + (i.getProgress() + "%"));
-    }
-  }
-
-  @Override
-  public void handleStateChangeNotification(Preloader.StateChangeNotification info) {
-    StateChangeNotification.Type type = info.getType();
-    if (Objects.requireNonNull(type) == Type.BEFORE_START) {
-      preloaderStage.hide();
     }
 
-  }
+    @Override
+    public void init() throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+            RiskPreloader.class.getResource("fxml/splashScreen.fxml"));
+        scene = new Scene(fxmlLoader.load());
+        splashScreenController = fxmlLoader.getController();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.preloaderStage = stage;
+        preloaderStage.getIcons().add(new Image(Objects.requireNonNull(RisikoMain.class.getResourceAsStream("pictures/logo.png"))));
+        preloaderStage.setScene(scene);
+        preloaderStage.initStyle(StageStyle.UNDECORATED);
+        preloaderStage.show();
+    }
+
+    @Override
+    public void handleApplicationNotification(Preloader.PreloaderNotification info) {
+        if (info instanceof ProgressNotification i) {
+            splashScreenController.getProgress()
+                .setText("Version 1.1        Loading " + (i.getProgress() + "%"));
+        }
+    }
+
+    @Override
+    public void handleStateChangeNotification(Preloader.StateChangeNotification info) {
+        StateChangeNotification.Type type = info.getType();
+        if (Objects.requireNonNull(type) == Type.BEFORE_START) {
+            preloaderStage.hide();
+        }
+
+    }
 
 }
